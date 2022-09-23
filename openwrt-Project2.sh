271320a4e170e275ee9bb1b130a38722b8e6a4b7
acer168 <114073340+acer168@users.noreply.github.com>
2022-09-23 13:30:00 +0800

    Update openwrt-Project2.sh

--- openwrt-Project2.sh
+++ openwrt-Project2.sh
@@ -19,13 +19,13 @@
 
 
 # 修改固件默认名称
-sed -i 's/OpenWrt/NEWIFI/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/NEWIFI/g' package/base-files/files/bin/config_generate
 
 # 修改固件默认时区
-sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+# sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
 
 # 管理地址
-sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/files/bin/config_generate
 
 # 默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
