185952b2837fb9aec878223006f6995269a14800
acer168 <114073340+acer168@users.noreply.github.com>
2022-09-23 12:16:39 +0800

    Update openwrt-Project2.sh

--- openwrt-Project2.sh
+++ openwrt-Project2.sh
@@ -17,6 +17,13 @@
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+
+# 修改固件默认名称
+sed -i 's/OpenWrt/NEWIFI/g' package/base-files/files/bin/config_generate
+
+# 修改固件默认时区
+sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
+
 # 管理地址
 sed -i 's/192.168.1.1/192.168.99.2/g' package/base-files/files/bin/config_generate
 
@@ -25,9 +32,3 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci
 
 # 修改NAS为网络存储
 sed -i 's/"NAS"/"网络存储"/g' `grep "NAS" -rl ./`
-
-# 修改固件默认时区
-sed -i 's/UTC/CST-8/g' package/base-files/files/bin/config_generate
-
-# 修改固件默认名称
-sed -i 's/OpenWrt/NEWIFI/g' package/base-files/files/bin/config_generate
